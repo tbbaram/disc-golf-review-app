dd07b595b48ef7bd9bd67a2c6157d01451119e70
Taylor <bKuzzcs@gmail.com>
2023-08-13 14:00:26 -0600

    working json

--- src/components/CoursesContainer.jsx
+++ src/components/CoursesContainer.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function CoursesContainer() {
+    
+    // const [courses, setCourses] = useState([])
+
+        useEffect(() => {
+            fetch("http://localhost:3000/discgolfcourses")
+            .then(r => r.json())
+            .then(data => console.log(data))
+        }, [])
+    
+
     return (
         <div>
             <h3>Life is good now</h3>
