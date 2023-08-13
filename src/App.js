630b6669c4b7e5ad9db3796671e90f60a9747a15
Taylor <bKuzzcs@gmail.com>
2023-08-13 13:30:52 -0600

    json data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from './components/NavBar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import CoursesContainer from './components/CoursesContainer';
 
